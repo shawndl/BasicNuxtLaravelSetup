0ab95cf71965e24e4a4f4374d6786164ed95df9b
shawndl <shawnswebsites@gmail.com>
2018-04-13 19:15:31 +1000

    removed routes from web

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    $a = \App\Image::first();
-    dd($a->path);
-//    factory(\App\LocationType::class, 10)->create();
-//    factory(\App\Role::class)->create(['name' => 'admin']);
-//    factory(\App\User::class)->create(['email' => 'test@gmail.com', 'password' => bcrypt('aaaaaa')]);
-
-    return view('welcome');
-});
